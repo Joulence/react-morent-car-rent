191b0115d8caaf737fb066b2c8e3a83b82817bab
Joulence <4erviken@gmail.com>
2023-06-05 10:46:26 +0300

    Added container class

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,16 @@
-import { Fragment } from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
 
 const RootLayout = () => {
   return (
-    <Fragment>
-      <Outlet />
-    </Fragment>
+    <div className="container">
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
