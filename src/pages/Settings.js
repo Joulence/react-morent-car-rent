d6c8a265d4ecc3155218ab230ee02f8fbf2ca1d2
Joulence <4erviken@gmail.com>
2023-06-05 10:18:17 +0300

    Added Settings page

---
+++ src/pages/Settings.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const SettingsPage = () => {
+  return (
+    <Fragment>
+        <h1>Settings Page</h1>
+    </Fragment>
+  );
+};
+
+export default SettingsPage;
