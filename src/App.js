77e2f5f35ddb90b2042070391dc2bc72af43a09b
Joulence <4erviken@gmail.com>
2023-06-05 10:17:09 +0300

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import RootLayout from "./pages/Root";
+import NotificationsPage from "./pages/Notifications";
+import SettingsPage from "./pages/Settings";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "notifications",
+        element: <NotificationsPage />,
+      },
+      {
+        path: "settings",
+        element: <SettingsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    'React'
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
