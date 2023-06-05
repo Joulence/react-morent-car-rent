29d3f0041991cb0fef985efb4560d28ff806083c
Joulence <4erviken@gmail.com>
2023-06-05 10:45:40 +0300

    Created layout for header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,17 @@
-const Header = () => {
+import classes from "./Header.module.css";
 
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes['header__menu']}>
+        <h2 className={classes['header__menu-logo']}>morent</h2>
+        <div className={classes['header__menu-search']}>
+            <input type="text" />
+        </div>
+      </div>
+      <div className={classes['header__buttons']}>settings</div>
+    </header>
+  );
 };
 
 export default Header;
