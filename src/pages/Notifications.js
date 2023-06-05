7349da4a587c6ea3a564cb4dea24da300e2af8b9
Joulence <4erviken@gmail.com>
2023-06-05 10:17:43 +0300

    Added Notifications page

---
+++ src/pages/Notifications.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+
+const NotificationsPage = () => {
+  return (
+    <Fragment>
+        <h1>Notifications Page</h1>
+    </Fragment>
+  );
+};
+
+export default NotificationsPage;
