01d8e8a7568a590a5100d5c7982bf3f0dc230fa4
Joulence <4erviken@gmail.com>
2023-06-06 14:17:06 +0300

    Created BannerCard UI

---
+++ src/components/UI/BannerCard.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import styles from "./BannerCard.module.css";
+
+const BannerCard = (props) => {
+    const darkTheme = `${styles.banner} ${styles.dark}`;
+  const theme = `${styles.banner} ${props.dark ? darkTheme : ''}`;
+  return (
+    <div className={theme}>
+      <h3>{props.title}</h3>
+      <div>{props.text}</div>
+      <Link>Rental car</Link>
+      <img src={props.img} alt="banner-car" />
+    </div>
+  );
+};
+
+export default BannerCard;
