037849ae282b6e5642090c1470b0fc75143180f6
Joulence <4erviken@gmail.com>
2023-06-07 11:34:59 +0300

    Fixed spaces and added dynamic link to banners

--- src/components/UI/BannerCard.js
+++ src/components/UI/BannerCard.js
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import styles from "./BannerCard.module.css";
 
 const BannerCard = (props) => {
-    const darkTheme = `${styles.banner} ${styles.dark}`;
-  const theme = `${styles.banner} ${props.dark ? darkTheme : ''}`;
+  const darkTheme = `${styles.banner} ${styles.dark}`;
+  const theme = `${styles.banner} ${props.dark ? darkTheme : ""}`;
   return (
     <div className={theme}>
       <h3>{props.title}</h3>
       <div>{props.text}</div>
-      <Link>Rental car</Link>
+      <Link to={props.link}>Rental car</Link>
       <img src={props.img} alt="banner-car" />
     </div>
   );
