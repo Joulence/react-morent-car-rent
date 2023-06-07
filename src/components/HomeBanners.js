36e4ba153d32be537f21ed5871b8a5a2bef30c09
Joulence <4erviken@gmail.com>
2023-06-07 11:33:30 +0300

    Banners' logic was moved to its own component

---
+++ src/components/HomeBanners.js
@@ -0,0 +1,27 @@
+import BannerCard from "./UI/BannerCard";
+import bannerCarFirst from "../assets/images/banners/banner1.png";
+import bannerCarSecond from "../assets/images/banners/banner2.png";
+import styles from "./HomeBanners.module.css";
+
+const HomeBanners = () => {
+  return (
+    <div className={styles.banners}>
+      <BannerCard
+        title="The Best Platform for Car Rental"
+        text="Ease of doing a car rental safely and reliably. Of course at a low price."
+        img={bannerCarFirst}
+        link="cars/car1"
+      />
+
+      <BannerCard
+        title="Easy way to rent a car at a low price"
+        text="Providing cheap car rental services and safe and comfortable facilities."
+        img={bannerCarSecond}
+        link="cars/car2"
+        dark={true}
+      />
+    </div>
+  );
+};
+
+export default HomeBanners;
