ca5b17e6f54cd3874f173b22a90eb81108aeaca3
Joulence <4erviken@gmail.com>
2023-06-07 11:36:09 +0300

    All logic was moved into different components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,11 @@
-import BannerCard from "../components/UI/BannerCard";
-import styles from "./Home.module.css";
-import bannerCarFirst from "../assets/images/banners/banner1.png";
-import bannerCarSecond from "../assets/images/banners/banner2.png";
+import HomeBanners from "../components/HomeBanners";
+import HomeList from "../components/HomeList";
 
 const HomePage = () => {
   return (
     <div className="container">
-      <div className={styles.banners}>
-        <BannerCard
-          title="The Best Platform for Car Rental"
-          text="Ease of doing a car rental safely and reliably. Of course at a low price."
-          img={bannerCarFirst}
-        />
-
-        <BannerCard
-          title="Easy way to rent a car at a low price"
-          text="Providing cheap car rental services and safe and comfortable facilities."
-          img={bannerCarSecond}
-          dark={true}
-        />
-      </div>
+      <HomeBanners />
+      <HomeList />
     </div>
   );
 };
