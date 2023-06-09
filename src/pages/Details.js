6885b16ee63ab77e16d6ff51ec6c30af1909a2b2
Joulence <4erviken@gmail.com>
2023-06-09 22:51:56 +0300

    Outputting info by ID

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,4 +1,34 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/use-fetch";
+// import styles from "./Details.module.css";
+
 const DetailsPage = () => {
-    return <h1>Details page</h1>
-}
+  const params = useParams();
+  const { data, isLoading, httpError } = useFetch(
+    "description",
+    `https://react-morent-car-rent-default-rtdb.firebaseio.com/cars/${params.carId}.json`
+  );
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const { name, description, price, link, fuel, people, discount, type, gear } =
+    data;
+
+  return <div className="container">{name}</div>;
+};
+
 export default DetailsPage;
