65336a229db172204c5cc856d69c649964a5c137
Joulence <4erviken@gmail.com>
2023-06-10 19:31:38 +0300

    Updated Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,25 @@
+import { Fragment } from "react";
+import Header from "../components/Layout/Header";
+import Footer from "../components/Layout/Footer";
+
 const ErrorPage = () => {
-    return <h1>Error</h1>
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <div className="container">
+          <section>
+            <h2>Ooops.... Error occured!</h2>
+            <p>
+              The page you're looking for may have been moved, deleted, or it
+              could simply be a mistyped URL.
+            </p>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default ErrorPage;
