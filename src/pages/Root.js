ad0803819776bd5e46cd3d187d17630be910a659
Joulence <4erviken@gmail.com>
2023-06-10 15:27:53 +0300

    Created element to prevent scroll bug

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
 import { Fragment } from "react";
+import ScrollToTop from "../utils/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <Fragment>
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
