e2fb648989addf310db3b77c7d41424780316994
Joulence <4erviken@gmail.com>
2023-06-10 19:41:31 +0300

    Created Terms Page

---
+++ src/pages/Terms.js
@@ -0,0 +1,109 @@
+const PolicyPage = () => {
+  return (
+    <div className="container">
+      <section>
+        <h2>Terms & Conditions</h2>
+        <p>
+          Welcome to the Terms & Conditions page of Morent. These terms outline
+          the agreement between you (the user) and Morent (the company)
+          regarding the use of our website and services. By accessing or using
+          our website, making a reservation, or interacting with our services,
+          you agree to comply with and be bound by the following terms. Please
+          read them carefully:
+        </p>
+        <ol>
+          <li>
+            <h3>Reservation and Rental:</h3>
+            When making a reservation, you must provide accurate and complete
+            information. The rental terms, including rates, availability, and
+            specific conditions, will be clearly stated during the reservation
+            process. By confirming your reservation, you agree to the terms and
+            conditions associated with the rental.
+          </li>
+
+          <li>
+            <h3>Driver's License and Eligibility:</h3>
+            You must possess a valid driver's license and meet the minimum age
+            requirements set by the rental location. Additional drivers may also
+            need to meet certain criteria. It is your responsibility to ensure
+            compliance with these requirements.
+          </li>
+
+          <li>
+            <h3>Vehicle Usage:</h3>
+            You agree to use the rental vehicle responsibly, following all
+            applicable traffic laws and regulations. The vehicle should not be
+            used for illegal activities, racing, off-road driving, or any other
+            unauthorized purposes. You are responsible for any fines, penalties,
+            or damages incurred during the rental period.
+          </li>
+
+          <li>
+            <h3>Payment and Charges:</h3>
+            You are responsible for all charges associated with your rental,
+            including the rental fees, additional services, fuel, tolls, and any
+            applicable taxes. Payment should be made using accepted methods as
+            specified during the reservation process.
+          </li>
+
+          <li>
+            <h3>Insurance and Liability:</h3>
+            Rental vehicles may be covered by insurance, subject to specific
+            terms and conditions. It is your responsibility to review and
+            understand the coverage provided. You may have the option to
+            purchase additional insurance or waivers for added protection.
+          </li>
+
+          <li>
+            <h3>Cancellations and Modifications:</h3>
+            Cancellation and modification policies vary depending on the
+            reservation details, rental location, and applicable terms. It is
+            important to review the specific policies provided during the
+            reservation process and adhere to the stated requirements.
+          </li>
+
+          <li>
+            <h3>Intellectual Property:</h3>
+            All content, logos, trademarks, and intellectual property displayed
+            on our website are the property of Morent or its affiliates.
+            Unauthorized use, reproduction, or distribution of this content is
+            strictly prohibited.
+          </li>
+
+          <li>
+            <h3>Limitation of Liability:</h3>
+            Morent is not liable for any direct, indirect, incidental,
+            consequential, or special damages arising from the use of our
+            website or services, including but not limited to vehicle damage,
+            personal injury, or financial loss.
+          </li>
+
+          <li>
+            <h3>Governing Law and Jurisdiction:</h3>
+            These terms and conditions are governed by and construed in
+            accordance with the laws of the jurisdiction in which Morent
+            operates. Any disputes arising from these terms shall be subject to
+            the exclusive jurisdiction of the courts in that jurisdiction.
+          </li>
+        </ol>
+
+        <p>
+          Please note that these terms and conditions are subject to change, and
+          it is your responsibility to review them periodically for updates. By
+          continuing to use our website and services, you acknowledge and accept
+          any revisions to the terms and conditions.
+        </p>
+
+        <p>
+          If you have any questions or concerns regarding our terms and
+          conditions, please contact our customer support team. We are here to
+          assist you and ensure a clear understanding of our agreement
+        </p>
+
+        <p>Last updated: [10.06.2023]</p>
+      </section>
+    </div>
+  );
+};
+
+export default PolicyPage;
