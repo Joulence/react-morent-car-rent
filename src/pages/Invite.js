dd7daa076040dfc6d6497786ebcc3da782657612
Joulence <4erviken@gmail.com>
2023-06-10 19:26:11 +0300

    Created Invite Page

---
+++ src/pages/Invite.js
@@ -0,0 +1,91 @@
+const InvitePage = () => {
+  return (
+    <div className="container">
+      <section>
+        <h2>Invite a Friend</h2>
+        <p>
+          Thank you for considering inviting a friend to experience Morent's
+          exceptional car rental services. Sharing your positive experiences and
+          recommendations with others not only benefits your friends but also
+          helps us expand our customer base. Here's how you can invite a friend
+          to join Morent:
+        </p>
+        <ol>
+          <li>
+            <h3>Personalized Referral Link:</h3>
+            We provide a personalized referral link that you can share with your
+            friends. This link will direct them to a special landing page where
+            they can learn more about Morent and sign up for an account.
+          </li>
+
+          <li>
+            <h3>Exclusive Benefits for Referrals:</h3>
+            To show our appreciation for your referral, we offer exclusive
+            benefits for both you and your friend. These benefits may include
+            discounts on rentals, bonus loyalty points, or other promotional
+            offers that enhance the value of their car rental experience.
+          </li>
+
+          <li>
+            <h3>Sharing Options:</h3>
+            You can easily share your referral link through various channels,
+            including email, social media platforms, messaging apps, or by
+            simply copying and pasting the link into a communication of your
+            choice.
+          </li>
+
+          <li>
+            <h3>Personalized Invitation:</h3>
+            Along with the referral link, we encourage you to include a
+            personalized invitation message. Share your positive experiences
+            with Morent, highlighting the exceptional service, wide vehicle
+            selection, and hassle-free rental process. Personal recommendations
+            often carry more weight and can make your friend feel confident
+            about trying Morent.
+          </li>
+
+          <li>
+            <h3>Tracking Referrals:</h3>
+            We have a system in place to track referrals and ensure that you
+            receive the applicable rewards for successfully referring a friend.
+            You'll be notified once your friend signs up and completes their
+            first rental, and your benefits will be credited accordingly.
+          </li>
+
+          <li>
+            <h3>Sharing the Morent Experience:</h3>
+            Encourage your friend to explore our website, browse our vehicle
+            options, and take advantage of the features and benefits we offer.
+            They can easily search for their desired rental location, select the
+            dates, and choose from our well-maintained and reliable vehicles.
+          </li>
+
+          <li>
+            <h3>Customer Support:</h3>
+            Assure your friend that our dedicated customer support team is
+            available to assist them throughout their rental experience. Whether
+            they have questions, need assistance with bookings, or require any
+            other support, our team is ready to provide top-notch service.
+          </li>
+        </ol>
+
+        <p>
+          Thank you for considering sharing the Morent experience with your
+          friends. By inviting them to join, you're not only introducing them to
+          a reliable and customer-centric car rental service but also helping us
+          grow and improve. Start inviting your friends today and let them
+          experience the convenience, quality, and satisfaction that Morent
+          offers.
+        </p>
+
+        <p>
+          Note: Specific referral programs and benefits may vary, so please
+          refer to our official referral program details for the most accurate
+          and up-to-date information.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default InvitePage;
