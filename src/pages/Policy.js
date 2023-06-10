a14e981133c1a4b51de2727882cd17060f9f99e0
Joulence <4erviken@gmail.com>
2023-06-10 19:37:40 +0300

    Created Policy Page

---
+++ src/pages/Policy.js
@@ -0,0 +1,96 @@
+const PolicyPage = () => {
+  return (
+    <div className="container">
+      <section>
+        <h2>Privacy & Policy</h2>
+        <p>
+          Welcome to the Privacy & Policy page of Morent. We are committed to
+          protecting your privacy and ensuring the security of your personal
+          information. This page outlines our policies and practices regarding
+          the collection, use, and disclosure of your data when you visit our
+          website or use our services. Please take a moment to familiarize
+          yourself with the following key points:
+        </p>
+        <ol>
+          <li>
+            <h3>Information Collection:</h3>
+            We may collect personal information such as your name, contact
+            details, and payment information when you create an account, make a
+            reservation, or interact with our website. Additionally, we may
+            gather non-personal information such as IP addresses, browser type,
+            and device information for analytical purposes.
+          </li>
+
+          <li>
+            <h3>Data Usage:</h3>
+            The information we collect is primarily used to provide you with our
+            car rental services, process reservations, and improve our website's
+            functionality. We may also use your data to communicate with you,
+            send promotional offers or newsletters, and personalize your user
+            experience.
+          </li>
+
+          <li>
+            <h3>Data Security:</h3>
+            We implement strict security measures to protect your personal
+            information from unauthorized access, alteration, or disclosure. Our
+            systems are regularly monitored, and we use industry-standard
+            encryption technologies to safeguard your data during transmission.
+          </li>
+
+          <li>
+            <h3>Data Sharing:</h3>
+            We may share your information with trusted third parties, such as
+            payment processors or service providers, to facilitate our
+            operations and deliver our services effectively. However, we do not
+            sell or rent your personal information to third parties for
+            marketing purposes.
+          </li>
+
+          <li>
+            <h3>Cookies and Tracking Technologies:</h3>
+            Our website may use cookies and similar tracking technologies to
+            enhance your browsing experience, analyze website traffic, and
+            personalize content. You can manage your cookie preferences through
+            your browser settings.
+          </li>
+
+          <li>
+            <h3>Third-Party Links:</h3>
+            Our website may contain links to third-party websites or services
+            that are not operated or controlled by Morent. We are not
+            responsible for the privacy practices or content of these external
+            sites. We encourage you to review their privacy policies before
+            interacting with them.
+          </li>
+
+          <li>
+            <h3>Compliance with Laws:</h3>
+            We adhere to applicable privacy laws and regulations, and we
+            continuously monitor changes in the legal landscape to ensure our
+            practices remain compliant.
+          </li>
+        </ol>
+
+        <p>
+          By using our website and services, you consent to the collection, use,
+          and storage of your information as described in our Privacy & Policy.
+          We reserve the right to update or modify this policy from time to
+          time, and any changes will be reflected on this page.
+        </p>
+
+        <p>
+          If you have any questions, concerns, or requests regarding your
+          privacy or our data practices, please don't hesitate to contact our
+          Privacy & Policy team. We value your privacy and are dedicated to
+          maintaining a transparent and secure environment for your interactions
+          with Morent.
+        </p>
+
+        <p>Last updated: [10.06.2023]</p>
+      </section>
+    </div>
+  );
+};
+
+export default PolicyPage;
